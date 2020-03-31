42e3f16c4745795aacbe9b492e224b0414cad023
idshklein <idshklein@gmail.com>
2020-03-31 21:00:28 +0300

    Ido's first try of Population commit

---
+++ matsim-jerusalem/src/jerusalem/scenario/population/Runner.java
@@ -0,0 +1,13 @@
+package jerusalem.scenario.population;
+
+import java.io.IOException;
+
+import jerusalem.scenario.network.CreateNetwork;
+
+public class Runner {
+
+	public static void main(String[] args) throws IOException {
+		CreateNetwork.main(args);
+	}
+
+}
