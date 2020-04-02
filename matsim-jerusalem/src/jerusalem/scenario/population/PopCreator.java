fc9fe539c2aaefd7530bf4cffa710aa866867070
idshklein <idshklein@gmail.com>
2020-04-02 16:06:19 +0300

    run sql population query from seprate file

--- matsim-jerusalem/src/jerusalem/scenario/population/PopCreator.java
+++ matsim-jerusalem/src/jerusalem/scenario/population/PopCreator.java
@@ -1,5 +1,9 @@
 package jerusalem.scenario.population;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -28,52 +32,78 @@ import org.matsim.core.scenario.ScenarioUtils;
  */
 public class PopCreator {
 
+	private final static String INPUT_POPULATION_CSV = "D:/Users/User/Dropbox/matsim_begin/population.xml";
+
 	public static void main(String[] args) {
 //		TODO create local database through java
 		String url = "jdbc:postgresql://localhost:5432/postgres";
 		String user = "postgres";
 		String password = "matsim";
+		BufferedReader br = null;
+		String query = "";
+		String line = "";
+		try {
+			br = new BufferedReader(new FileReader("./sql_scripts/pop_query.sql"));
 
+			while ((line = br.readLine()) != null) {
+				query = query + line;
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		System.out.println(query);
 		try (Connection con = DriverManager.getConnection(url, user, password);
-				PreparedStatement pst = con.prepareStatement(
-						"SELECT t.hhid,t.pnum,t.personTripNum,t.origTaz,t.destTaz,t.origPurp,t.destPurp,t.finalDepartMinute,t.modeCode,\r\n"
-								+ "		tzo.X AS origX,tzo.Y AS origY,\r\n" + "		tzd.X AS destX,tzd.Y AS destY,\r\n"
-								+ "		MAX(t.personTripNum) OVER (PARTITION BY t.hhid,t.pnum) AS lastTripNum\r\n"
-								+ "FROM trips AS t\r\n" + "LEFT JOIN TAZ_coordinates as tzo\r\n"
-								+ "ON t.origTaz = tzo.taz\r\n" + "LEFT JOIN TAZ_coordinates as tzd\r\n"
-								+ "ON t.destTaz = tzd.taz\r\n" + "ORDER BY t.hhid,t.pnum,t.personTripNum;");
-				ResultSet rs = pst.executeQuery()) {
+				PreparedStatement pst = con.prepareStatement(query);
+				ResultSet resultSet = pst.executeQuery()) {
 			Config config = ConfigUtils.createConfig();
 			Scenario sc = ScenarioUtils.createScenario(config);
 			Network network = sc.getNetwork();
 			Population population = sc.getPopulation();
 			PopulationFactory populationFactory = population.getFactory();
-//			useless definition of person and plan
+
+			// useless definition of person and plan
 			Person person = populationFactory.createPerson(Id.create("1", Person.class));
 			Plan plan = populationFactory.createPlan();
 			int i = 0;
-			while (rs.next()) {
-//				creating a new person if one started
-				if (rs.getInt("personTripNum") == 1) {
-					String agentId = rs.getString("hhid") + "-" + rs.getString("pnum");
+			while (resultSet.next()) {
+
+				// creating a new person if one started
+				if (resultSet.getInt("personTripNum") == 1) {
+					String agentId = resultSet.getString("hhid") + "-" + resultSet.getString("pnum");
 					person = populationFactory.createPerson(Id.create(agentId, Person.class));
 					population.addPerson(person);
 					plan = populationFactory.createPlan();
 				}
-//				getting parameters from table
-				Coord origCoordinates = new Coord(rs.getDouble("origX"), rs.getDouble("origY"));
-				String actType = PopUtils.ActivityType(rs.getInt("origPurp"));
-				double endTime = rs.getDouble("finalDepartMinute") * 60 + 10800;
-				String mode = PopUtils.Mode(rs.getInt("modeCode"));
-//				adding activity and leg
+
+				// getting parameters from table
+				Coord origCoordinates = new Coord(resultSet.getDouble("origX"), resultSet.getDouble("origY"));
+				String actType = PopUtils.ActivityType(resultSet.getInt("origPurp"));
+				double endTime = resultSet.getDouble("finalDepartMinute") * 60 + 10800;
+				String mode = PopUtils.Mode(resultSet.getInt("modeCode"));
+
+				// adding activity and leg
 				Activity activity = populationFactory.createActivityFromCoord(actType, origCoordinates);
 				activity.setEndTime(endTime);
 				plan.addActivity(activity);
 				plan.addLeg(populationFactory.createLeg(mode));
-//				last activity - adding person to population
-				if (rs.getInt("personTripNum") == rs.getInt("lastTripNum")) {
-					Coord destCoordinates = new Coord(rs.getDouble("destX"), rs.getDouble("destY"));
-					actType = PopUtils.ActivityType(rs.getInt("destPurp"));
+
+				// last activity - adding person to population
+				if (resultSet.getInt("personTripNum") == resultSet.getInt("lastTripNum")) {
+					Coord destCoordinates = new Coord(resultSet.getDouble("destX"), resultSet.getDouble("destY"));
+					actType = PopUtils.ActivityType(resultSet.getInt("destPurp"));
 					activity = populationFactory.createActivityFromCoord(actType, destCoordinates);
 					plan.addActivity(activity);
 					person.addPlan(plan);
@@ -81,9 +111,10 @@ public class PopCreator {
 				System.out.println(i);
 				i++;
 			}
-//			writing to file
+
+			// writing to file
 			MatsimWriter popWriter = new PopulationWriter(population, network);
-			popWriter.write("D:/Users/User/Dropbox/matsim_begin/population.xml");
+			popWriter.write(INPUT_POPULATION_CSV);
 
 		} catch (SQLException ex) {
 
