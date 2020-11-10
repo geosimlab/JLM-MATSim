0f3fae3804166fa64fbf3ac67372ae24b0ff5e9a
Ido klein <idshklein@gmail.com>
2020-11-10 08:27:05 +0200

    attempt to add turn restrictions

---
+++ matsim-jerusalem/src/jerusalem/scenario/network/TurnsManeuverRunner.java
@@ -0,0 +1,20 @@
+package jerusalem.scenario.network;
+
+import java.sql.SQLException;
+
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.network.io.NetworkWriter;
+
+public class TurnsManeuverRunner
+{
+	public static void main(String[] args) throws Exception {
+//		https://github.com/idshklein/Storage/blob/master/java/playgrounds/toronto/src/main/java/playground/toronto/maneuvers/ManeuverCreation.java
+		String emmeFilePath = "D:/matsim_jlm/input/misc/EmmeManeuverRestrictions.csv";
+		String networkFilePath ="D:/matsim_jlm/output/11.network.xml.gz";
+		NetworkAddEmmeManeuverRestrictions test = new NetworkAddEmmeManeuverRestrictions(emmeFilePath);
+		Network network = NetworkUtils.readNetwork(networkFilePath);
+		test.run(network);
+		new NetworkWriter(network).write("D:/matsim_jlm/output/12.network.xml.gz");
+	}
+}
